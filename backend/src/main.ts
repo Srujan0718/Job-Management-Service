04279e1078101ac88b7019a803ca2914bb7bff0e
Srujan0718 <vtu20155@veltech.edu.in>
2025-08-25 16:59:25 +0530

    making changes for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,13 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   
   app.useGlobalPipes(new ValidationPipe());
   
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();

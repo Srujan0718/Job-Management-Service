6177143d7c62247a69663769c63a57c739f7446a
Srujan0718 <vtu20155@veltech.edu.in>
2025-08-25 17:35:00 +0530

    resolved supabase ipv6 connection issue

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,20 +11,16 @@ import { Job } from './jobs/entities/job.entity';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-      type: 'postgres',
-      host: config.get<string>('DATABASE_HOST'), // e.g. db.xxxxx.supabase.co
-      port: 5432,
-      username: config.get<string>('DATABASE_USERNAME'), // usually 'postgres'
-      password: config.get<string>('DATABASE_PASSWORD'), // the DB password, not service key
-      database: config.get<string>('DATABASE_NAME'), // usually 'postgres'
-      entities: [Job],
-      schema: 'public',
-      ssl: true,
-      extra: {
-        ssl: { rejectUnauthorized: false },
-      },
-      synchronize: false,
-    }),
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        entities: [Job],
+        schema: 'public',
+        ssl: true,
+        extra: {
+          ssl: { rejectUnauthorized: false },
+        },
+        synchronize: false,
+      }),
     }),
     JobsModule,
   ],
